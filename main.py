a649ddfa94fd0dbcd238f9c3833299236ba0c5b3
esther mensah <edmworkmail@gmail.com>
2025-06-23 11:00:24 +0200

    project updates to use the always active version instead on reservable version

--- main.py
+++ main.py
@@ -13,11 +13,25 @@ with open("inventory.json") as f:
 os.makedirs("backups", exist_ok=True)
 
 for device in devices:
-
     try:
         connection = ConnectHandler(**device)
         output = connection.send_command("show running-config")
-        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        filename = f"backups/{device['host']}_{timestamp}.txt"
+        timestamp = datetime.now().strftime("%Y%M%d_%H%M%S")
+        filename = f"backups/{device['host']}_backup_{timestamp}.txt"
+
+        with open(filename, "w") as file:
+            file.write(output)
+
+            print(f"Backup saved: {filename}")
+            connection.disconnect()
+
+
+    except NetMikoTimeoutException:
+        print(f"Connection timed out for device: {device['host']}")
+    except NetMikoAuthenticationException:
+        print(f"Authentication failed for device: {device['host']}")
+    except Exception as e:
+        print(f"Other error with device {device['host']}: {e}")
+
 
         
